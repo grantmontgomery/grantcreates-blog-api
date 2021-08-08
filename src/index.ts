182e888ec77b1a8266231c931d038c34ca8af358
Grant <gmontgomery1994@gmail.com>
2021-08-07 22:20:31 -0700

    attached schema to apollo server.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,21 @@ import "reflect-metadata";
 import express from "express";
 import { createConnection, getRepository } from "typeorm";
 import cors from "cors";
+import { UserResolver } from "./resolvers/UserResulver";
 import { ApolloServer, gql } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
+import { buildSchema, emitSchemaDefinitionFileSync } from "type-graphql";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
 
 const startServer = async () => {
   try {
@@ -11,14 +24,26 @@ const startServer = async () => {
     app.use(cors());
     app.use(express.json());
     const schema = await buildSchema({
-      resolvers: [__dirname + "/resolvers/*.ts"],
+      resolvers: [
+        __dirname + "/**/*.resolvers.ts",
+        __dirname + "/resolvers/**/*.ts",
+        __dirname + "/resolvers/**/*.js",
+      ],
+      emitSchemaFile: true,
     });
+
+    // const server = new ApolloServer({ schema });
 
     const server = new ApolloServer({ schema });
 
     await server.start();
 
-    await createConnection();
+    server.applyMiddleware({ app });
+
+    const connection = await createConnection();
+
+    console.log(connection);
+
     app.listen({ port: 3001 }, () => {
       console.log("Server ready at 3001");
     });
